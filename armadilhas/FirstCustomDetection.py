3a5b030bf51db210d2ef5e891da98f5741ccf8ca
Gabriel <>
2024-04-18 13:57:26 -0300

    .

--- armadilhas/FirstCustomDetection.py
+++ armadilhas/FirstCustomDetection.py
@@ -6,7 +6,7 @@ detector.setModelTypeAsYOLOv3()
 detector.setModelPath("armadilhas/insetos/models/yolov3_insetos_mAP-0.51397_epoch-34.pt")
 detector.setJsonPath("armadilhas/insetos/json/insetos_yolov3_detection_config.json")
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/lagarta_teste4.jpg", output_image_path="armadilhas/novos_testes2/lagarta_teste4-detected-new2.jpg")
+detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/lagarta_teste2.jpg", output_image_path="armadilhas/novos_testes2/lagarta_teste2-detected-new2.jpg")
 for detection in detections:
     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
 
