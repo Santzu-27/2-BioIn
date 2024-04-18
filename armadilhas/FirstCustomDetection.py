876fb3086ab6b55c04c5a1d9ab0fbcc24c75b895
Gabriel <>
2024-04-18 13:45:07 -0300

    Teste mudando os batchs

--- armadilhas/FirstCustomDetection.py
+++ armadilhas/FirstCustomDetection.py
@@ -6,7 +6,7 @@ detector.setModelTypeAsYOLOv3()
 detector.setModelPath("armadilhas/insetos/models/yolov3_insetos_mAP-0.51397_epoch-34.pt")
 detector.setJsonPath("armadilhas/insetos/json/insetos_yolov3_detection_config.json")
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/insetos_teste1.jpg", output_image_path="armadilhas/novos_testes/insetos_teste1-detected-new2.jpg")
+detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/lagarta_teste4.jpg", output_image_path="armadilhas/novos_testes2/lagarta_teste4-detected-new2.jpg")
 for detection in detections:
     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
 
