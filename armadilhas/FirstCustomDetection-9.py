239f2fca2fb302296b3ca304360cdb7398e4ff53
Gabriel <>
2024-04-22 16:00:18 -0300

    Novos treinos

--- armadilhas/FirstCustomDetection-9.py
+++ armadilhas/FirstCustomDetection-9.py
@@ -6,7 +6,7 @@ detector.setModelTypeAsYOLOv3()
 detector.setModelPath("armadilhas/insetos/models/yolov3_insetos_mAP-0.05754_epoch-9.pt")
 detector.setJsonPath("armadilhas/insetos/json/insetos_yolov3_detection_config.json")
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/insetos_teste2.jpg", output_image_path="armadilhas/novos_testes_arquivos/epoch-9/insetos_teste2.jpg-detected-1.jpg")
+detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/lagarta_teste1.jpg", output_image_path="armadilhas/novos_testes_arquivos/epoch-9/lagarta_teste1.jpg-detected-1.jpg")
 for detection in detections:
     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
 
--- armadilhas/train1.py
+++ armadilhas/train1.py
@@ -3,7 +3,7 @@ from imageai.Detection.Custom import DetectionModelTrainer
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
 trainer.setDataDirectory(data_directory="armadilhas/insetos")
-trainer.setTrainConfig(object_names_array=["insetos"], batch_size=2, num_experiments=10)
+trainer.setTrainConfig(object_names_array=["insetos"], batch_size=6, num_experiments=90, train_from_pretrained_model="yolov3_insetos_mAP-0.54737_epoch-30.pt" )
 # In the above,when training for detecting multiple objects,
 #set object_names_array=["object1", "object2", "object3",..."objectz"]
 trainer.trainModel()
