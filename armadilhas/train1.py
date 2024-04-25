60ad6a27cd81f815b9aaca8bf7bd0f25f4270d72
Gabriel <>
2024-04-25 12:59:29 -0300

    mudanã de paramentros
    E pretrained model

--- armadilhas/train1.py
+++ armadilhas/train1.py
@@ -3,7 +3,7 @@ from imageai.Detection.Custom import DetectionModelTrainer
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
 trainer.setDataDirectory(data_directory="armadilhas/insetos")
-trainer.setTrainConfig(object_names_array=["insetos"], batch_size=4, num_experiments=220, train_from_pretrained_model="/home/oficinas40/2-BioIn/armadilhas/insetos/models/yolov3.pt")
+trainer.setTrainConfig(object_names_array=["insetos"], batch_size=6, num_experiments=250, train_from_pretrained_model="/home/oficinas40/2-BioIn/armadilhas/insetos/models/yolov3_insetos_mAP-0.54737_epoch-30.pt")
 # In the above,when training for detecting multiple objects,
 #set object_names_array=["object1", "object2", "object3",..."objectz"]
 trainer.trainModel()
