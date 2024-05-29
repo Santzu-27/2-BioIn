a9368afae96013dfe99740d409fb64999ddde45a
Santzu-27 <gabi260402@gmail.com>
2024-05-29 12:21:20 -0300

    Novas Imagens

--- armadilhas/train1.py
+++ armadilhas/train1.py
@@ -3,7 +3,7 @@ from imageai.Detection.Custom import DetectionModelTrainer
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
 trainer.setDataDirectory(data_directory="armadilhas/insetos")
-trainer.setTrainConfig(object_names_array=["tuta-absoluta", "lagarta"], batch_size=5, num_experiments=120)
+trainer.setTrainConfig(object_names_array=["tuta-absoluta", "lagarta"], batch_size=6, num_experiments=130)
 # In the above,when training for detecting multiple objects,
 #set object_names_array=["object1", "object2", "object3",..."objectz"]
 trainer.trainModel()
