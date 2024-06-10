328518c60d14435bfa7a5faccc1ddbbc5c065c65
Santzu-27 <108244286+Santzu-27@users.noreply.github.com>
2024-06-10 08:49:40 -0300

    config

--- armadilhas/train.py
+++ armadilhas/train.py
@@ -3,5 +3,5 @@ from imageai.Detection.Custom import DetectionModelTrainer
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
 trainer.setDataDirectory(data_directory="armadilhas/insetos")
-trainer.setTrainConfig(object_names_array=["insetos"], batch_size=6, num_experiments=150)
+trainer.setTrainConfig(object_names_array=["tuta-absoluta", "lagarta"], batch_size=5, num_experiments=120)
 trainer.trainModel()
