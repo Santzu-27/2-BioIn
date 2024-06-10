350b872592bfb01d0574ebd97881c942e005122b
Santzu-27 <108244286+Santzu-27@users.noreply.github.com>
2024-06-10 09:21:50 -0300

    classes att

--- armadilhas/FirstCustomDetection-34.py
+++ armadilhas/FirstCustomDetection-34.py
@@ -9,5 +9,3 @@ detector.loadModel()
 detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/insetos_teste1.jpg", output_image_path="armadilhas/novos_testes_arquivos/epoch-34/insetos_teste1-detected-1.jpg")
 for detection in detections:
     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
-
-
