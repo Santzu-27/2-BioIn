60628228aa2fb184f179c91e73a6e114f5af357c
Santzu-27 <gabi260402@gmail.com>
2024-06-28 18:37:38 -0300

    novos testes

--- armadilhas/detections/FirstCustomDetection-classes.py
+++ armadilhas/detections/FirstCustomDetection-classes.py
@@ -3,10 +3,10 @@ from imageai.Detection.Custom import CustomObjectDetection
 
 detector = CustomObjectDetection()
 detector.setModelTypeAsYOLOv3()
-detector.setModelPath("armadilhas/insetos/models/yolov3_insetos_mAP-0.24854_epoch-46.pt")
+detector.setModelPath("armadilhas/insetos/models/yolov3_insetos_mAP-0.25185_epoch-38.pt")
 detector.setJsonPath("armadilhas/insetos/json/insetos_yolov3_detection_config.json")
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/insetos_teste1.jpg", output_image_path="armadilhas/novos_testes_arquivos/classes-separadas/insetos_teste1.jpg-detected-1.jpg")
+detections = detector.detectObjectsFromImage(input_image="restante/399.jpg", output_image_path="armadilhas/novos_testes_arquivos/classes-separadas/mpc5.jpg-detected-1.jpg")
 for detection in detections:
     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
 
