642b7e2c34755a82c6be71569fd1dc2cb68c3c61
Santzu-27 <108244286+Santzu-27@users.noreply.github.com>
2024-07-02 20:34:37 -0300

    novas imagens

--- armadilhas/detections/Detection-769.py
+++ armadilhas/detections/Detection-769.py
@@ -6,7 +6,7 @@ detector.setModelTypeAsYOLOv3()
 detector.setModelPath("armadilhas/insetos/models/mAP-0.29539_epoch-769.pt")
 detector.setJsonPath("armadilhas/insetos/json/insetos_yolov3_detection_config.json")
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/insetos_teste1.jpg", output_image_path="armadilhas/novos_testes_arquivos/epoch-769/insetos_teste1-detected-1.jpg")
+detections = detector.detectObjectsFromImage(input_image="armadilhas/novos_testes_arquivos/epoch-769/20.jpg", output_image_path="armadilhas/novos_testes_arquivos/epoch-769/20-sem.jpg")
 for detection in detections:
     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
 
