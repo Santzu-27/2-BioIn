cf703e713779fe2c4d884c9c4bb3e036c93b3058
Santzu-27 <gabi260402@gmail.com>
2024-07-08 00:31:00 -0300

    novas detections

--- armadilhas/detections/4class-Detection-414-885.py
+++ armadilhas/detections/4class-Detection-414-885.py
@@ -6,7 +6,7 @@ detector.setModelTypeAsYOLOv3()
 detector.setModelPath("armadilhas/insetos/models/0.36705_414-885.pt")
 detector.setJsonPath("armadilhas/insetos/json/insetos_yolov3_detection_config.json")
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/insetos_teste2.jpg", output_image_path="armadilhas/novos_testes_arquivos/epoch-414/insetos_teste2-detected.jpg")
+detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/lagarta_teste1.jpg", output_image_path="armadilhas/novos_testes_arquivos/epoch-414/lagarta_teste1-detected-name.jpg")
 for detection in detections:
     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
 
