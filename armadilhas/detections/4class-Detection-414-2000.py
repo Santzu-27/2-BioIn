2a874df2a14284df675405330d632f92762b85b5
Santzu-27 <gabi260402@gmail.com>
2024-08-23 11:48:56 -0300

    att

--- armadilhas/detections/4class-Detection-414-2000.py
+++ armadilhas/detections/4class-Detection-414-2000.py
@@ -6,7 +6,7 @@ detector.setModelTypeAsYOLOv3()
 detector.setModelPath("armadilhas/insetos/models/0.36705_epoch-414.pt")
 detector.setJsonPath("armadilhas/insetos/json/insetos_yolov3_detection_config.json")
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/lagarta_teste4.jpg", output_image_path="armadilhas/novos_testes_arquivos/epoch-414/2-lagarta_teste4-detected-name.jpg")
+detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/instetos_teste1.jpg", output_image_path="armadilhas/novos_testes_arquivos/epoch-414/2-insetos_Teste.jpg")
 for detection in detections:
     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
 
--- armadilhas/detections/Detection-_08_08-135.py
+++ armadilhas/detections/Detection-_08_08-135.py
@@ -6,6 +6,6 @@ detector.setModelTypeAsYOLOv3()
 detector.setModelPath("armadilhas/insetos/models/e135-0.20478_epoch-25.pt")
 detector.setJsonPath("armadilhas/insetos/json/insetos_yolov3_detection_config.json")
 detector.loadModel()
-detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/lagarta_teste2.jpg", output_image_path="armadilhas/novos_testes_arquivos/9500-exp/la4-38de135.jpg")
+detections = detector.detectObjectsFromImage(input_image="armadilhas/teste/inseto_teste2.jpg", output_image_path="armadilhas/novos_testes_arquivos/9500-exp/in2-38de135.jpg")
 for detection in detections:
     print(detection["name"], " : ", detection["percentage_probability"], " : ", detection["box_points"])
